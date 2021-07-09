b3bd08b66d250c45f19e77d5242c8bf9f55ba6f8
WillyPess <willypessoa12@gmail.com>
2021-07-09 16:25:26 -0300

    Primeiro Commit
    Projeto Do will

---
+++ TestProject2/Script/HomeScreen.js
@@ -0,0 +1,11 @@
+﻿
+var HomeScreen = function(){
+  
+  
+  this.clickNovaOrdem = async() => {
+  let botaoNovaOrdem = Aliases.Orders.MainForm.MainMenu;
+  botaoNovaOrdem.Click("Orders|New order...");
+  }
+  
+}
+module.exports = new HomeScreen;
---
+++ TestProject2/Script/InsereNovaOrdem.js
@@ -0,0 +1,14 @@
+﻿var novaOrdem = require("NovaOrdemScreen");
+var homeScreen = require("HomeScreen");
+
+function InsereNovaOrdem()
+{
+  
+//  homeScreen.clickNovaOrdem();
+  novaOrdem.fillQuantidade();
+  novaOrdem.fillTotal();
+  novaOrdem.fillPrecoUnidade();
+  novaOrdem.fillDesconto();
+  novaOrdem.fillData();
+  
+  }
